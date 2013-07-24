007966476514c14814289843eea2059b50ecbb23
siddharth00@gmail.com <siddharth00@gmail.com>
2013-07-23 22:44:50 -0700

    adding support for conn string

--- MvcWebRole1/WebRole.cs
+++ MvcWebRole1/WebRole.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using DataStoreLib.Utils;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
@@ -13,6 +15,10 @@ namespace MvcWebRole1
         {
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            var connectionString = CloudConfigurationManager.GetSetting("StorageTableConnectionString");
+            Trace.TraceInformation("Connection str read");
+            ConnectionSettingsSingleton.Instance.StorageConnectionString = connectionString;  
 
             return base.OnStart();
         }
