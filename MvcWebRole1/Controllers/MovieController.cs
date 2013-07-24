0dd504ac0db28b384b41f93e17f55e5ce0d4c9bd
gautama <gautama@outlook.com>
2013-07-24 00:33:01 -0700

    UX movie skeleton

---
+++ MvcWebRole1/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebRole1.Controllers
+{
+    public class MovieController : Controller
+    {
+        //
+        // GET: /Movie/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
